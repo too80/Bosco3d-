0f155e05b2677cba3eca5b61605737fb1f45c551
too80 <giampiero.olivares@gmail.com>
2026-07-07 13:31:20 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bosco3D Lite v1.1
+
+Logo compagnia, data/ora, salvataggio manuale del risultato e storico allenamenti.
--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-const CACHE_NAME="bosco3d-lite-v1";
-const FILES=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME="bosco3d-lite-v1-1";const FILES=["./","./index.html","./style.css","./app.js","./manifest.webmanifest","./icon.svg","./logo-compagnia.jpg"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting()});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
